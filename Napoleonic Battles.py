6b6895568c3f10ad7f70ad23cf8194836a24fedf
SamuelEddon <98049171+SamuelEddon@users.noreply.github.com>
2022-01-19 18:21:23 +0000

    Add files via upload

---
+++ Napoleonic Battles.py
@@ -0,0 +1,3 @@
+country = input("Tell me a European country in existense between 1798-1815")
+if country == "Great Britain" or "United Kingdom":
+    print("Famous Napoleonic British Battles include Trafalgar in 1805, Salamanca in 1812, Vitoria in 1813 and Waterloo in 1815")
