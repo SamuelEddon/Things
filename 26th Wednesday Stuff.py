fe6c447039bddeb3b3515a0946615e8e6b6b4e39
SamuelEddon <98049171+SamuelEddon@users.noreply.github.com>
2022-01-26 17:24:18 +0000

    Add files via upload

---
+++ 26th Wednesday Stuff.py
@@ -0,0 +1,6 @@
+word = input("Say a word please")
+dorw = input("Say the word backwards")
+if dorw == word:
+    print("Congratulations, that is a palindrome")
+else:
+    print("That is not a palindrome")
